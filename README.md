6c4cc33203c9aa5cd71e5e7555e045ac3df30da6
zyzweb <zyz1609062132@gmail.com>
2022-05-28 15:25:44 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-2x1hte
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-2x1hte)
---
+++ main.js
@@ -0,0 +1,6 @@
+import './style.css';
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hello Vite3333!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`;
